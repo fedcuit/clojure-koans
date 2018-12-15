eb62a980e21041b73975aa6439ea3df7d5e0742d
Erdong Feng <edfeng@thoughtworks.com>
2018-12-15 16:25:08 +0800

    equalities/strings/lists

--- project.clj
+++ project.clj
@@ -5,6 +5,6 @@
   :dev-dependencies [[lein-koan "0.1.4"]]
   :profiles {:dev {:dependencies [[lein-koan "0.1.4"]]}}
   :repl-options {:init-ns koan-engine.runner
-                 :init ^:displace (do (use '[koan-engine.core]))}
+                 :init    ^:displace (do (use '[koan-engine.core]))}
   :plugins [[lein-koan "0.1.4"]]
   :main koan-engine.runner/exec)
